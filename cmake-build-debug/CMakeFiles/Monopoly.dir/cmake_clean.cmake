cdc1ae7d1f97f84a5802cb5be037e11d3720cd09
Nicolas BERT <nbert@centrale-marseille.fr>
2021-12-29 16:10:57 +0100

    j'ajoute tout et on voit si ça marche

---
+++ cmake-build-debug/CMakeFiles/Monopoly.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Monopoly.dir/main.cpp.o"
+  "Monopoly.exe"
+  "Monopoly.pdb"
+  "libMonopoly.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Monopoly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Monopoly.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Monopoly.
+# This may be replaced when dependencies are built.
