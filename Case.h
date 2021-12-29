2fa4cbdafbc399d29b92b3def29588e36ccaf46d
Arthur L <arthur.louradou@centrale-marseille.fr>
2021-12-29 18:43:23 +0100

    :sparkles: Added case and plateau

---
+++ Case.h
@@ -0,0 +1,36 @@
+//
+// Created by Arthur on 29/12/2021.
+//
+
+#ifndef MONOPOLY_CASE_H
+#define MONOPOLY_CASE_H
+
+#import <iostream>
+using namespace std;
+
+
+class Case {
+private:
+    string nom;
+    Case suivante;
+
+public:
+    Case();
+
+    void arreterSur();
+    string getNom(){
+        return this->nom;
+    }
+    void setNom(string nom){
+        this->nom = nom;
+    }
+    Case getSuivante(){
+        return this->suivante;
+    }
+    void setSuivante(Case suiv){
+        this->suivante = suiv;
+    }
+};
+
+
+#endif //MONOPOLY_CASE_H
---
+++ Plateau.h
@@ -0,0 +1,15 @@
+//
+// Created by Arthur on 29/12/2021.
+//
+
+#ifndef MONOPOLY_PLATEAU_H
+#define MONOPOLY_PLATEAU_H
+
+
+class Plateau {
+private:
+    Case case;
+};
+
+
+#endif //MONOPOLY_PLATEAU_H
